d90f5ecc6cd59b74daaa17b8973f8dcf93632da1
MandapatisureshQA <114691739+MandapatisureshQA@users.noreply.github.com>
2023-01-29 06:46:40 +0530

    s

--- js/app.js
+++ js/app.js
@@ -31,3 +31,40 @@ const manualCloseSidebar = () => {
 
 manualHamburger.addEventListener("click", manualOpenSidebar );
 manualCloseButton.addEventListener("click", manualCloseSidebar);
+
+//nav bar sticky
+
+const navbarContainer = document.querySelector(".navbar-container");
+window.addEventListener("scroll", function()
+{
+    const scrollNumber = window.scrollY;
+    const targetNumber = window.innerWidth  >= 992 ? 70 : 50 ;
+    if(scrollNumber >= targetNumber)
+    {
+        navbarContainer.classList.add("sticky-nav");
+    }
+    else
+    {
+        navbarContainer.classList.remove("sticky-nav");
+    }
+});
+
+
+//mibile sidebar sticky
+
+const navbarContainer1 = document.querySelector(".hero-manual-header");
+window.addEventListener("scroll", function()
+{
+    const scrollNumber = window.scrollY;
+    const targetNumber = window.innerWidth  >= 992 ? 70 : 50 ;
+    if(scrollNumber >= targetNumber)
+    {
+        navbarContainer1.classList.add("sticky-nav");
+    }
+    else
+    {
+        navbarContainer1.classList.remove("sticky-nav");
+    }
+});
+
+
